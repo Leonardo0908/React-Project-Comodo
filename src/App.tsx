476a945fe081a5b1df46c5aef472eea2c9b52389
Leonardo E S Rodrigues <leonardo.elias0908@gmail.com>
2022-08-17 20:44:42 -0300

    adding React Router Dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes  } from 'react-router-dom';
 import './App.css';
 import Footer from './components/estaticos/footer/Footer';
 import Navbar from './components/estaticos/navbar/Navbar';
@@ -7,9 +8,19 @@ import Home from './components/pages/home/Home';
 function App() {
   return (
       <>
-       <Navbar />
-          <Home />
-       <Footer />
+        <Router>
+        <Navbar />
+        <div style={{ minHeight: '100vh' }} >
+        <Routes>
+          
+          <Route path="/" element={<Home />} />
+
+          <Route path="/home" element={<Home />} />
+       
+        </Routes>
+        </div>
+        <Footer />
+      </Router>
       </>
   );
 }
