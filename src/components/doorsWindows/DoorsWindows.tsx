add65bd44a5b556538346ed7b1a0859299887005
Leonardo E S Rodrigues <leonardo.elias0908@gmail.com>
2022-08-18 18:09:04 -0300

    door and window function

--- src/components/doorsWindows/DoorsWindows.tsx
+++ src/components/doorsWindows/DoorsWindows.tsx
@@ -1,37 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import { Grid, Button } from '@mui/material';
 import "./DoorsWindows.css";
 
 function DoorsWindows() {
+
+    const [doors, setDoors] = useState(0);  // Portas;
+    const [windows, setWindows] = useState(0);   // Janelas
+
+    // Menos Porta
+    function LessDoor() {
+        setDoors(doors - 1);
+    }
+
+    // Mais Porta
+    function MoreDoor() {
+        setDoors(doors + 1);
+    }
+
+    // Menos Janela
+    function LessWindow() {
+            setWindows(windows - 1)
+    }
+
+    // Mais Janela 
+    function MoreWindow() {
+            setWindows(windows + 1) 
+    }
+
     return (
         <>
             <Grid container direction="row" >
 
                 <h4>Doors</h4>
 
-                <Button>
+                <Button onClick={LessDoor} >
                     <h3>-</h3>
                 </Button>
-
-                <Button>
+                <h1>{doors}</h1>
+                <Button onClick={MoreDoor} >
                     <h3>+</h3>
                 </Button>
-                
+
                 <h4>Windows</h4>
 
-                <Button>
+                <Button onClick={LessWindow} >
                     <h3>-</h3>
                 </Button>
-
-                <Button>
+                    <h1>{windows}</h1>
+                <Button onClick={MoreWindow} >
                     <h3>+</h3>
                 </Button>
 
             </Grid>
-            
 
-                
-         
+
+
+
         </>
     );
 }
